5cc5b12f2495d8c09a30b9f1c10e34811ecb77e9
Shanmukhayya <sanmukswamy238@gmail.com>
2024-03-14 15:27:42 +0530

    implemented swagger get and post methods

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('swagger-jsdoc');
 const app = express();
 
 //importing app middleware
@@ -8,6 +10,27 @@ app.use(express.urlencoded({extended:true}));
 //connected BD
 require('dotenv').config();
 require('./DB');
+
+// Define Swagger options
+const options = {
+    swaggerDefinition: {
+        info: {
+            title: 'API Documentation',
+            version: '1.0.0',
+            description: 'API documentation for your Express.js application',
+        },
+    },
+    host: 'localhost:5700', // Your server's hostname
+    basePath: '/', // The base path on which the API is served, which is optional.
+    apis: ['./routes/routes.js'], // Path to the API routes
+};
+
+// Initialize Swagger-jsdoc
+const specs = swaggerDoc(options);
+
+// Serve Swagger UI at /api-docs endpoint
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(specs));
+
 
 //imporing routing middleware
 const router = require('./routes/routes');
--- routes/routes.js
+++ routes/routes.js
@@ -4,11 +4,61 @@ const controller = require('../controller/controller');
 const registerdStudents = require('../controller/registerdStudents');
 
 //registerd details
+
+/**
+ * Handle POST request to create a new post.
+ * @swagger
+ * /register:
+ *   post:
+ *     summary: Create a new post
+ *     description: Create a new post with a specified schema.
+ *     produces:
+ *       - application/json
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: user
+ *         schema:
+ *           type: object
+ *           properties:
+ *             name:
+ *               type: string
+ *               description: The post content.
+ *             email:
+ *               type: string
+ *               description: Remarks for the post.
+ *             password:
+ *               type: string
+ *               description: User ID associated with the post.
+ *     responses:
+ *       200:
+ *         description: Post created successfully.
+ *         content:
+ *           application/json:
+ *             example: { message: 'Post created successfully' }
+ */
 routes.post('/register',controller.register);
 routes.post('/login',controller.login);
 routes.put('/update/:id',controller.update);
+routes.delete('/dalete/:id',controller.deleteData);
+
+
+/**
+ * @swagger
+ * /getData:
+ *   get:
+ *     summary: Get all getData
+ *     description: Retrieve a list of all getData
+ *     responses:
+ *       200:
+ *         description: data
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ */
 routes.get('/getData',controller.getAllData);
-routes.delete('/dalete/:id',controller.deleteData)
 
 
 
